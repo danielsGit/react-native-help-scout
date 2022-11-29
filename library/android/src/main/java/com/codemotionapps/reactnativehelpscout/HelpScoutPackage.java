df169c9e08bd6a1c46516ee4cbc9774c10a44f6e
William Toney Hall <devkinggod@gmail.com>
2022-11-29 10:00:49 -0500

    Update for android 31

--- library/android/src/main/java/com/codemotionapps/reactnativehelpscout/HelpScoutPackage.java
+++ library/android/src/main/java/com/codemotionapps/reactnativehelpscout/HelpScoutPackage.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -16,7 +17,7 @@ public class HelpScoutPackage implements ReactPackage {
 	}
 
 	@Override
-	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-		return Collections.emptyList();
+	public List<Class<? extends JavaScriptModule>> createJSModules() {
+		return null;
 	}
 }
